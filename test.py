9fce24c4577ab49c6617cbf31d51598c134565f9
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-05-31 01:14:17 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,16 +2,20 @@ import cv2
 from mtcnn import MTCNN
 import numpy as np
 import tensorflow
+from scipy.spatial import distance
 
 model = tensorflow.keras.models.load_model("D:\Face_Mask_Detection_\VGG19.h5", compile=False)
-
 face_cascade = cv2.CascadeClassifier("D:\Face_Mask_Detection_\haarcascades\haarcascade_frontalface_default.xml") # đường dẫn đến tệp XML của Haar Cascade
-#detector = MTCNN()
+MIN_DISTANCE = 130
 video_capture = cv2.VideoCapture(0) # mở camera
-labels = {0: 'Mask', 1: 'NoMask'}
+
+mask_label = {0:'MASK',1:'NO MASK'}
+dist_label = {0:(0,255,0),1:(255,0,0)}
+
 if not video_capture.isOpened():
     print ("Could not open cam")
     exit()
+
 while True:
     # Read a frame from the video
     ret, frame = video_capture.read()
@@ -19,24 +23,37 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_resized = cv2.resize(gray, (128, 128))
     img_color = cv2.cvtColor(gray_resized, cv2.COLOR_GRAY2RGB)
-    img_reshape = np.reshape(img_color, (1, 128, 128, 3))
-    predict = model.predict(img_reshape)
-    print(predict)
+    img_reshape = np.reshape(img_color, (1, 128, 128, 3))/255.0
+
+    # predict = model.predict(img_reshape)
+    # print(predict)
     
-    prediction_index = np.argmax(predict, axis=-1)[0]
-    prediction_label = labels[prediction_index]
-    #   results = detector.detect_faces(frame)
-    print(prediction_label)
+    # prediction_index = np.argmax(predict, axis=-1)[0]
+    # prediction_label = labels[prediction_index]
+    # #   results = detector.detect_faces(frame)
+    # print(prediction_label)
 
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame, prediction_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-    # for result in results:
-    #     x, y, w, h = result['box']
+    # for (x, y, w, h) in faces:
     #     cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
     #     cv2.putText(frame, prediction_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    if len(faces)>=2:
+        label = [0 for i in range(len(faces))]
+        for i in range(len(faces)-1):
+            for j in range(i+1, len(faces)):
+                dist = distance.euclidean(faces[i][:2],faces[j][:2])
+                if dist<MIN_DISTANCE:
+                    label[i] = 1
+                    label[j] = 1
+    new_img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    for i in range(len(faces)):
+        (x,y,w,h) = faces[i]
+        crop = new_img[y:y+h,x:x+w]
+        crop = cv2.resize(crop,(128,128))
+        crop = np.reshape(crop,[1,128,128,3])/255.0
+        mask_result = model.predict(crop)
+        cv2.putText(new_img,mask_label[mask_result.argmax()],(x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,dist_label[label[i]],2)
+        cv2.rectangle(new_img,(x,y),(x+w,y+h),dist_label[label[i]],1)
     cv2.imshow("face",frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
