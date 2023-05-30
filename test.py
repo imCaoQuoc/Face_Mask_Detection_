c6f20ace107f6306f9cda2364384d71f4f66b183
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-05-31 01:40:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,9 @@ import cv2
 from mtcnn import MTCNN
 import numpy as np
 import tensorflow
-from scipy.spatial import distance
 
 model = tensorflow.keras.models.load_model("D:\Face_Mask_Detection_\VGG19.h5", compile=False)
 face_cascade = cv2.CascadeClassifier("D:\Face_Mask_Detection_\haarcascades\haarcascade_frontalface_default.xml") # đường dẫn đến tệp XML của Haar Cascade
-MIN_DISTANCE = 130
 video_capture = cv2.VideoCapture(0) # mở camera
 labels = {0: 'Mask', 1: 'NoMask'}
 if not video_capture.isOpened():
@@ -22,15 +20,17 @@ while True:
         face = gray[y:y+h, x:x+w]
         face = cv2.resize(face, (128, 128))
         face = cv2.cvtColor(face, cv2.COLOR_GRAY2RGB)
-        print(face.shape)
         face = np.reshape(face, [1, 128, 128, 3])/255.0
         predict = model.predict(face)
         print(predict)
         prediction_index = np.argmax(predict, axis=-1)[0]
         prediction_label = labels[prediction_index]
-        print(prediction_label)
+        if predict > 0.5:
+            label = 'With Mask'
+        else:
+            label = 'Without Mask'
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame, prediction_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
     cv2.imshow("face",frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
