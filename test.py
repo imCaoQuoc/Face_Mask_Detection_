4a93c16a3e5ddaee9152f07f13bc88b8802c7b9c
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-06-02 18:03:51 +0700

    Update test.py

--- test.py
+++ test.py
@@ -39,6 +39,7 @@
 
 import cv2
 import av
+import tempfile
 import numpy as np
 import streamlit as st
 import tensorflow as tf
@@ -53,36 +54,36 @@ labels = {0: 'Mask', 1: 'NoMask'}
 model = tf.keras.models.load_model(MODEL_PATH, compile= False)
 face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
 
-class VideoProcessor:
-    def recv(self, frame):
-        frm = frame.to_ndarray(format="bgr24")
-        # Convert frame to grayscale
-        gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+# Use this line to capture video from the webcam
+cap = cv2.VideoCapture(0)
 
-        # Detect faces in the frame
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+# Set the title for the Streamlit app
+st.title("Video Capture with OpenCV")
 
-        for (x, y, w, h) in faces:
-            face = gray[y:y + h, x:x + w]
-            face = cv2.resize(face, (224, 224))
-            face = cv2.cvtColor(face, cv2.COLOR_GRAY2RGB)
-            face = np.reshape(face, [1, 224, 224, 3]) / 255.0
+frame_placeholder = st.empty()
 
-            # Predict face mask
-            predict = model.predict(face)
-            prediction_index = np.argmax(predict, axis=-1)[0]
-            prediction_label = labels[prediction_index]
+# Add a "Stop" button and store its state in a variable
+stop_button_pressed = st.button("Stop")
 
-            cv2.rectangle(frm.to_ndarray(format="bgr24"), (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frm.to_ndarray(format="bgr24"), prediction_label, (x, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+while cap.isOpened() and not stop_button_pressed:
+    ret, frame = cap.read()
 
-        return av.VideoFrame.from_ndarray(frm, format='bgr24')
+    if not ret:
+        st.write("The video capture has ended.")
+        break
 
+    # You can process the frame here if needed
+    # e.g., apply filters, transformations, or object detection
 
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
-				rtc_configuration=RTCConfiguration(
-					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-					)
-	)
+    # Convert the frame from BGR to RGB format
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Display the frame using Streamlit's st.image
+    frame_placeholder.image(frame, channels="RGB")
+
+    # Break the loop if the 'q' key is pressed or the user clicks the "Stop" button
+    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed: 
+        break
+
+cap.release()
+cv2.destroyAllWindows()
