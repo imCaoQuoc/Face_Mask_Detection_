294f4adadcc0e7cacda3ad627dbb1a5282a428ad
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-08-23 15:49:01 +0700

    update MLfastAPI.py

--- MLfastAPI.py
+++ MLfastAPI.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 import cv2
 import numpy as np
 import tensorflow
@@ -9,8 +8,6 @@ app = FastAPI()
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 model = tensorflow.keras.models.load_model("MobileNet.h5", compile=False)
 labels = {0: 'Mask', 1: 'NoMask'}
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def home():
