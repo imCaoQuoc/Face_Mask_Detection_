17ee6b7069829e73ebb63a7d48e33839adee8762
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-08-23 15:21:03 +0700

    Update MLfastAPI.py

--- MLfastAPI.py
+++ MLfastAPI.py
@@ -21,6 +21,20 @@ def detect():
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
+        frm = frame.to_ndarray(format="bgr24")
+		gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+		faces = cascade.detectMultiScale(gray, 1.1, 5)
+
+        for x,y,w,h in faces:
+            face = gray[y:y+h, x:x+w]
+            face = cv2.resize(face, (224, 224))
+            face = cv2.cvtColor(face, cv2.COLOR_GRAY2RGB)
+            face = np.reshape(face, [1, 224, 224, 3])/255.0
+            predict = model.predict(face)
+            prediction_index = np.argmax(predict, axis=-1)[0]
+            prediction_label = labels[prediction_index]	
+            cv2.rectangle(frm, (x,y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(frm, prediction_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
         # Display the captured frame
         cv2.imshow("Camera Feed", frame)
