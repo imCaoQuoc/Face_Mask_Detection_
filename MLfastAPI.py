29be389e74b558ff336ee18ffa701b59f5e700ca
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-08-23 15:16:13 +0700

    trying fastapi

---
+++ MLfastAPI.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import cv2
+import numpy as np
+import tensorflow
+
+app = FastAPI()
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+model = tensorflow.keras.models.load_model("MobileNet.h5", compile=False)
+labels = {0: 'Mask', 1: 'NoMask'}
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Face Mask Detection system"}
+
+@app.get("/detect")
+def detect():
+    cv2.VideoCapture()
